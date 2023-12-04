2665cb7bcd8d0d904273eabaa14a87e82754f221
Josh Spicer <josh@joshspicer.com>
2023-12-04 15:44:18 +0000

    day 3 test

--- 03/main_test.go
+++ 03/main_test.go
@@ -5,10 +5,15 @@ import (
 	"testing"
 )
 
-func TestExample(t *testing.T) {
-	expected := "TODO"
-	result := run1(base.ReadExample1Lines())
-	if result != expected {
-		t.Errorf("ERR: %s != %s.", result, expected)
+func Test(t *testing.T) {
+	part1 := 527369
+	part2 := 73074886
+	actual1 := run1(base.ReadInputLines())
+	actual2 := run2(base.ReadInputLines())
+	if part1 != actual1 {
+		t.Errorf("ERR: %d != %d.", part1, actual1)
+	}
+	if part2 != actual2 {
+		t.Errorf("ERR: %d != %d.", part2, actual2)
 	}
 }
